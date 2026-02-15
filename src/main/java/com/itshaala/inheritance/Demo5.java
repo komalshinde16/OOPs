b6d1d7a2546d6199bd475cbb5e903ff477e47e7f
komalshinde16 <komalambadasshinde@gmail.com>
2026-02-15 18:50:15 +0530

    56

---
+++ src/main/java/com/itshaala/inheritance/Demo5.java
@@ -0,0 +1,9 @@
+package com.itshaala.inheritance;
+
+public class Demo5 {
+    public static void main(String[] args) {
+        class A{}
+        class B extends A{};
+        class C extends A{};
+    }
+}
---
+++ src/main/java/com/itshaala/inheritance/Demo6.java
@@ -0,0 +1,11 @@
+package com.itshaala.inheritance;
+
+public class Demo6 {
+    public static void main(String[] args) {
+        class A{}
+        class B extends A{};
+        class C extends A{};
+        class D extends A{};
+    }
+}
+
